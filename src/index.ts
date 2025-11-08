a2d7fb9c1c9c67b59cf892047cdeec887a7937ae
RodrigoRojas-dev <rojas.rodrigo.dev@gmail.com>
2025-11-08 20:24:08 -0300

    feat(server): Implementar servidor Express base y health check en la ruta principal (/).

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const PORT = process.env.PORT!
+
+const app = express()
+
+app.get("/", (__: Request, res: Response) => {
+  try {
+    res.status(200).json({success: true, data: "✅ ¡conexion exitosa!"})
+  } catch (e) {
+    const error = e as Error
+    res.status(500).json({success: false, data: `Error en el servidor: ${error.name}`})
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`✅ Servidor en escucha en el puerto http://localhost:${PORT}`)
+})
