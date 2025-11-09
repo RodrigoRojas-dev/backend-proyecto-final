f733ef3d248246de308d7e59bc18db4b1441aeec
RodrigoRojas-dev <rojas.rodrigo.dev@gmail.com>
2025-11-09 16:59:43 -0300

    feat(models): Crear modelo de Book.

---
+++ src/models/BookModel.ts
@@ -0,0 +1,26 @@
+import { model, Schema } from "mongoose";
+import { IBook } from "../interfaces/IBook";
+
+
+const bookSchema = new Schema<IBook>({
+  title: { type: String, required: true },
+  subtitle: { type: String, required: false },
+  author: { type: String, required: true },
+  synopsis: { type: String, required: true },
+  genre: { type: [String], required: true },
+  language: { type: String, required: true },
+  publisher: { type: String, required: true },
+  publicationDate: { type: Date, required: true },
+  pageCount: { type: Number, required: true, min: 1 },
+  format: { type: String, required: true, enum: ["Tapa Dura", "Tapa Blanda", "Ebook", "AudioLibro"] },
+  price: { type: Number, required: true, min: 0 },
+  stock: { type: Number, required: true, default: 0, min: 0 },
+  isAvailable: { type: Boolean, required: true }
+},
+  {
+    versionKey: false
+  })
+
+const Book = model<IBook>("Book", bookSchema)
+
+export { Book }
