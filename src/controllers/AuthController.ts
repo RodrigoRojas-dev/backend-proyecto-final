1209933ace48efbbf18794eb7f9f1f6b7a8b1094
RodrigoRojas-dev <rojas.rodrigo.dev@gmail.com>
2025-11-08 22:47:49 -0300

    feat(auth): Añadir endpoint POST /auth/login con validación de credenciales y generación de JWT.

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from "express";
 import bcrypt from "bcryptjs";
 import { User } from "../models/UserModel";
+import jwt from "jsonwebtoken";
 
-
+const SECRET_KEY = process.env.JWT_SECRET!
 
 class AuthController {
   static register = async (req: Request, res: Response) => {
@@ -21,6 +22,55 @@ class AuthController {
       switch (error.name) {
         case "MongoServerError":
           res.status(409).json({ success: false, error: "Usuario ya existe en nuestra base de datos." })
+          break;
+      }
+    }
+  }
+
+  static login = async (req: Request, res: Response) => {
+    try {
+      const { email, password } = req.body;
+
+      if (!email || !password) {
+        const error = new Error("Debe proporcionar email y contraseña.");
+        error.name = "InvalidInput";
+        throw error;
+      }
+
+      const user = await User.findOne({ email })
+
+      if (!user) {
+        const error = new Error("Credenciales no válidas.")
+
+        error.name = "AuthFail"
+
+        throw error
+      }
+
+      const isValid = await bcrypt.compare(password, user.password)
+
+      if (!isValid) {
+        const error = new Error("Credenciales no válidas.");
+        error.name = "AuthFail";
+        throw error;
+      }
+
+      const token = jwt.sign({ id: user._id }, SECRET_KEY, { expiresIn: "1h" })
+
+      res.status(200).json({ success: true, token })
+
+    } catch (e) {
+      const error = e as Error
+
+      switch (error.name) {
+        default:
+          res.status(500).json({ success: false, error: error.message })
+          break;
+        case "InvalidInput":
+          res.status(400).json({ success: false, error: error.message })
+          break;
+        case "AuthFail":
+          res.status(401).json({ success: false, error: error.message })
           break;
       }
     }
--- src/routes/AuthRouter.ts
+++ src/routes/AuthRouter.ts
@@ -5,5 +5,6 @@ import AuthController from "../controllers/AuthController";
 const authRouter = Router()
 
 authRouter.post("/register", AuthController.register)
+authRouter.post("/login", AuthController.login)
 
 export { authRouter }
